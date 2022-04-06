244002bfb9681fafd0c875e484d780515d0fce08
allanj <allanmcgrady@gmail.com>
2022-04-06 16:10:43 +0800

    robust config reading

--- src/config/config.py
+++ src/config/config.py
@@ -72,8 +72,8 @@ class Config:
         self.max_no_incre = args.max_no_incre
         self.max_grad_norm = args.max_grad_norm if "max_grad_norm" in args.__dict__ else None
 
-        self.print_detail_f1 = args.print_detail_f1
-        self.earlystop_atr = args.earlystop_atr
+        self.print_detail_f1 = args.print_detail_f1 if "print_detail_f1" in args.__dict__ else None
+        self.earlystop_atr = args.earlystop_atr if "earlystop_atr" in args.__dict__ else None
 
     def read_pretrain_embedding(self) -> Tuple[Union[Dict[str, np.array], None], int]:
         """
