dcd583e308d9732be67587bbda3c00de08df3e5f
austunel <austunel@newmind.ai>
2025-05-18 00:14:40 +0300

    refactor: enhance code formatting and readability in linear_encoder.py

--- src/model/module/linear_encoder.py
+++ src/model/module/linear_encoder.py
@@ -1,16 +1,16 @@
-
 import torch
 import torch.nn as nn
 
 
 class LinearEncoder(nn.Module):
-
-    def __init__(self, label_size:int, input_dim:int):
+    def __init__(self, label_size: int, input_dim: int):
         super(LinearEncoder, self).__init__()
 
         self.hidden2tag = nn.Linear(input_dim, label_size)
 
-    def forward(self, word_rep: torch.Tensor, word_seq_lens: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self, word_rep: torch.Tensor, word_seq_lens: torch.Tensor
+    ) -> torch.Tensor:
         """
         Encoding the input with BiLSTM
         :param word_rep: (batch_size, sent_len, input rep size)
@@ -19,5 +19,3 @@ class LinearEncoder(nn.Module):
         """
         outputs = self.hidden2tag(word_rep)
         return outputs
-
-
