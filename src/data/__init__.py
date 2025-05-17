85df72c4fd67006d0e6d6d8a0816a3398b1b9717
austunel <austunel@newmind.ai>
2025-05-18 00:14:29 +0300

    refactor: improve code formatting and readability across multiple files

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,4 +1,3 @@
-
 from src.data.instance import Instance
 from src.data.transformers_dataset import TransformersNERDataset
 from src.data.ner_dataset import NERDataset
--- src/data/ner_dataset.py
+++ src/data/ner_dataset.py
@@ -1,4 +1,4 @@
-# 
+#
 # @author: Allan
 #
 
@@ -8,22 +8,31 @@ from torch.utils.data import Dataset
 from torch.utils.data._utils.collate import default_collate
 import collections
 import numpy as np
-from src.data.data_utils import convert_iobes, build_label_idx, check_all_labels_in_dict, check_all_obj_is_None
+from src.data.data_utils import (
+    convert_iobes,
+    build_label_idx,
+    check_all_labels_in_dict,
+    check_all_obj_is_None,
+)
 
 from src.data import Instance
 from src.data.data_utils import UNK
 import re
 
-Feature = collections.namedtuple('Feature', 'words word_seq_len context_emb chars char_seq_lens labels')
+Feature = collections.namedtuple(
+    "Feature", "words word_seq_len context_emb chars char_seq_lens labels"
+)
 Feature.__new__.__defaults__ = (None,) * 6
 
 
 class NERDataset(Dataset):
-
-    def __init__(self, file: str,
-                 is_train: bool,
-                 label2idx: Dict[str, int] = None,
-                 number: int = -1):
+    def __init__(
+        self,
+        file: str,
+        is_train: bool,
+        label2idx: Dict[str, int] = None,
+        number: int = -1,
+    ):
         """
         Read the dataset into Instance
         """
@@ -38,11 +47,18 @@ class NERDataset(Dataset):
             self.idx2labels = idx2labels
             self.label2idx = label2idx
         else:
-            assert label2idx is not None ## for dev/test dataset we don't build label2idx, pass in label2idx argument
+            assert (
+                label2idx is not None
+            )  ## for dev/test dataset we don't build label2idx, pass in label2idx argument
             self.label2idx = label2idx
             check_all_labels_in_dict(insts=insts, label2idx=self.label2idx)
 
-    def convert_instances_to_feature_tensors(self, word2idx: Dict[str, int], char2idx: Dict[str, int], elmo_vecs: np.ndarray = None):
+    def convert_instances_to_feature_tensors(
+        self,
+        word2idx: Dict[str, int],
+        char2idx: Dict[str, int],
+        elmo_vecs: np.ndarray = None,
+    ):
         self.inst_ids = []
         for i, inst in enumerate(self.insts):
             words = inst.words
@@ -67,20 +83,26 @@ class NERDataset(Dataset):
                 for label in inst.labels:
                     output_ids.append(self.label2idx[label])
             context_emb = elmo_vecs[i] if elmo_vecs is not None else None
-            self.inst_ids.append(Feature(words = word_ids,
-                                         chars = char_ids,
-                                         word_seq_len = len(words),
-                                         char_seq_lens = char_seq_lens,
-                                         context_emb = context_emb,
-                                         labels = output_ids if inst.labels is not None else None))
-
-
+            self.inst_ids.append(
+                Feature(
+                    words=word_ids,
+                    chars=char_ids,
+                    word_seq_len=len(words),
+                    char_seq_lens=char_seq_lens,
+                    context_emb=context_emb,
+                    labels=output_ids if inst.labels is not None else None,
+                )
+            )
 
     def read_txt(self, file: str, number: int = -1) -> List[Instance]:
-        print(f"[Data Info] Reading file: {file}, labels will be converted to IOBES encoding")
-        print(f"[Data Info] Modify src/data/ner_dataset.read_txt function if you have other requirements")
+        print(
+            f"[Data Info] Reading file: {file}, labels will be converted to IOBES encoding"
+        )
+        print(
+            f"[Data Info] Modify src/data/ner_dataset.read_txt function if you have other requirements"
+        )
         insts = []
-        with open(file, 'r', encoding='utf-8') as f:
+        with open(file, "r", encoding="utf-8") as f:
             words = []
             ori_words = []
             labels = []
@@ -88,7 +110,9 @@ class NERDataset(Dataset):
                 line = line.rstrip()
                 if line == "":
                     labels = convert_iobes(labels)
-                    insts.append(Instance(words=words, ori_words=ori_words, labels=labels))
+                    insts.append(
+                        Instance(words=words, ori_words=ori_words, labels=labels)
+                    )
                     words = []
                     ori_words = []
                     labels = []
@@ -96,9 +120,9 @@ class NERDataset(Dataset):
                         break
                     continue
                 ls = line.split()
-                word, label = ls[0],ls[-1]
+                word, label = ls[0], ls[-1]
                 ori_words.append(word)
-                word = re.sub('\d', '0', word)
+                word = re.sub("\d", "0", word)
                 words.append(word)
                 labels.append(label)
         print("number of sentences: {}".format(len(insts)))
@@ -110,7 +134,7 @@ class NERDataset(Dataset):
     def __getitem__(self, index):
         return self.inst_ids[index]
 
-    def collate_fn(self, batch:List[Feature]):
+    def collate_fn(self, batch: List[Feature]):
         word_seq_lens = [len(feature.words) for feature in batch]
         max_seq_len = max(word_seq_lens)
         max_char_seq_len = -1
@@ -128,14 +152,26 @@ class NERDataset(Dataset):
                 chars.append(word_chars)
             for _ in range(max_seq_len - feature.word_seq_len):
                 chars.append([0] * max_char_seq_len)
-            labels = feature.labels + [0] * padding_length if feature.labels is not None else None
+            labels = (
+                feature.labels + [0] * padding_length
+                if feature.labels is not None
+                else None
+            )
 
-            batch[i] = Feature(words=np.asarray(words),
-                               chars=np.asarray(chars), char_seq_lens=np.asarray(char_seq_lens),
-                               context_emb = feature.context_emb,
-                               word_seq_len = feature.word_seq_len,
-                               labels= np.asarray(labels) if labels is not None else None)
-        results = Feature(*(default_collate(samples) if not check_all_obj_is_None(samples) else None for samples in zip(*batch) ))
+            batch[i] = Feature(
+                words=np.asarray(words),
+                chars=np.asarray(chars),
+                char_seq_lens=np.asarray(char_seq_lens),
+                context_emb=feature.context_emb,
+                word_seq_len=feature.word_seq_len,
+                labels=np.asarray(labels) if labels is not None else None,
+            )
+        results = Feature(
+            *(
+                default_collate(samples) if not check_all_obj_is_None(samples) else None
+                for samples in zip(*batch)
+            )
+        )
         return results
 
 
